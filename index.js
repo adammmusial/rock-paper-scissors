85699aa7a2c6120b7c2e45f343a09747c6154d3a
adammmusial <musialadam83@gmail.com>
2024-01-17 18:12:36 +0800

    feat: immplement playRound function to find out who won the game

--- index.js
+++ index.js
@@ -6,5 +6,44 @@ function getComputerChoice(){
     return randomString
 }
 
-console.log(getComputerChoice());
 
+function playRound(playerSelection){
+
+    computerSelection = getComputerChoice()
+    playerSelectionFiltered = playerSelection.toLowerCase().trim()
+
+    console.log(`You chose ${playerSelectionFiltered}`)
+    console.log(`Computer chose ${computerSelection}`)
+
+    if (playerSelectionFiltered === computerSelection){
+        return console.log("Tie! Play again to find out winner") 
+    }
+    else if (playerSelectionFiltered === "scissors" && computerSelection === "paper"){
+        return console.log("You win! Scissors beats paper")
+    }
+
+    else if (playerSelectionFiltered === "paper" && computerSelection === "rock"){
+        return console.log ("You win! Paper beats rock")
+    }
+
+    else if (playerSelectionFiltered === "rock" && computerSelection === "scissors"){
+        return console.log ("You win! Rock beats scissors")
+    }
+
+    else if (playerSelectionFiltered === "paper" && computerSelection === "scissors"){
+        return console.log ("You lose! Scissors beats paper")
+    }
+
+    else if (playerSelectionFiltered === "rock" && computerSelection === "paper"){
+        return console.log ("You lose! Paper beats rock")
+    }
+
+    else if (playerSelectionFiltered === "scissors" && computerSelection === "rock"){
+        return console.log ("You lose! Rock beats scissors")
+    }
+
+    return console.log("Inputted wrong name, please input: rock, paper or scissors")
+
+}
+
+console.log(game("paper"))
