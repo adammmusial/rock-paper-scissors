c6d0c373fb4b13ec6f54452c2b89bab9341e3ad7
adammmusial <musialadam83@gmail.com>
2024-01-17 19:18:54 +0800

    fix: simplify playRound function by removing multiple else ifs

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-function getComputerChoice(){
-    let options = ["rock","paper","scissors"];
-    let randomIndex = Math.floor(Math.random() * options.length );
+function getComputerChoice() {
+    let options = ["rock", "paper", "scissors"];
+    let randomIndex = Math.floor(Math.random() * options.length);
     let randomString = options[randomIndex];
 
     return randomString
 }
 
 
-function playRound(playerSelection){
+function playRound(playerSelection) {
 
     computerSelection = getComputerChoice()
     playerSelectionFiltered = playerSelection.toLowerCase().trim()
@@ -15,47 +15,35 @@ function playRound(playerSelection){
     console.log(`User chosse ${playerSelectionFiltered}`)
     console.log(`Computer chose ${computerSelection}`)
 
-    if (playerSelectionFiltered === computerSelection){
-        return console.log("Tie! Play again to find out winner") 
+    if (playerSelectionFiltered === computerSelection) {
+        console.log("Tie! Play again to find out winner")
+        return "Computer"
     }
-    else if (playerSelectionFiltered === "scissors" && computerSelection === "paper"){
-        return console.log("User wins! Scissors beats paper")
-    }
-
-    else if (playerSelectionFiltered === "paper" && computerSelection === "rock"){
-        return console.log ("User wins! Paper beats rock")
-    }
-
-    else if (playerSelectionFiltered === "rock" && computerSelection === "scissors"){
-        return console.log ("User wins! Rock beats scissors")
+    else if ((playerSelectionFiltered === "scissors" && computerSelection === "paper") ||
+        (playerSelectionFiltered === "paper" && computerSelection === "rock") ||
+        (playerSelectionFiltered === "rock" && computerSelection === "scissors")
+    ) {
+        console.log(`User wins! ${playerSelection} beats ${computerSelection}"`)
+        return "User"
     }
 
-    else if (playerSelectionFiltered === "paper" && computerSelection === "scissors"){
-        return console.log ("Computer Wins! Scissors beats paper")
+    else {
+        console.log("Computer Wins! Paper beats rock")
+        return "Computer"
     }
-
-    else if (playerSelectionFiltered === "rock" && computerSelection === "paper"){
-        return console.log ("Computer Wins! Paper beats rock")
-    }
-
-    else if (playerSelectionFiltered === "scissors" && computerSelection === "rock"){
-        return console.log ("Computer Wins! Rock beats scissors")
-    }
-
-    return console.log("Inputted wrong name, please input: rock, paper or scissors")
 
 }
 
-function game(){
+function game() {
     console.log("Hi, this is Rock Paper Scissor Game in order to win this game you have to score five points, good luck!")
 
-    let computerPoints = 0 ;
-    let userPoints = 0 ;
+    let computerPoints = 0;
+    let userPoints = 0;
     let tiePoints = 0
     let gameNumber = 1
 
-    for(let i=1; i<=9; i++){
-    
+    for (let i = 1; i <= 9; i++) {
+
         let choice = prompt(`Game number ${gameNumber}! Choose your choice by typing rock paper o scissors"`);
 
         gameNumber++
@@ -67,16 +55,16 @@ function game(){
 
         result = playRound(choice)
         result = String(result)
-        
-        if (result.includes("Computer")){
-            computerPoints++
+
+        if (result.includes("Computer")) {
+            computerPoints++;
         }
-        else if (result.includes("User")){
-            userPoints++
+        else if (result.includes("User")) {
+            userPoints++;
         }
-        else if (result.includes("Tie")){
-            tiePoints++
-        } 
+        else if (result.includes("Tie")) {
+            tiePoints++;
+        }
 
     }
     if (computerPoints === 5) {
