edcfb0691027c4058de0d4a76b5042d4c884bd35
adammmusial <musialadam83@gmail.com>
2024-01-17 17:43:47 +0800

    feat: create computerSelectio function to randomly select string from array

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('hello world')
+function computerSelection(){
+    let options = ["rock","paper","scissors"];
+    let randomIndex = Math.floor(Math.random() * options.length );
+    
+}
