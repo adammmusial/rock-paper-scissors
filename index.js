7bab16c5a67bd7e6f6b842c260f53c894d8c6136
adammmusial <musialadam83@gmail.com>
2024-01-17 17:48:56 +0800

    feat: add features to computerSelection function

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function computerSelection(){
     let options = ["rock","paper","scissors"];
     let randomIndex = Math.floor(Math.random() * options.length );
+    let randomString = options[randomIndex];
     
+    return randomString
 }
+
+console.log(computerSelection());
