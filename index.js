be959eb7d7589dde1f820c6cb6b4131795b8f81f
adammmusial <musialadam83@gmail.com>
2024-03-04 19:30:01 +0800

    feat: add EventListeners

--- index.js
+++ index.js
@@ -12,64 +12,63 @@ function playRound(playerSelection) {
     computerSelection = getComputerChoice()
     playerSelectionFiltered = playerSelection.toLowerCase().trim()
 
-    console.log(`User chosse ${playerSelectionFiltered}`)
-    console.log(`Computer chose ${computerSelection}`)
-
     if (playerSelectionFiltered === computerSelection) {
-        console.log("Tie! Play again to find out winner")
-        return "Computer"
-    }
-    else if ((playerSelectionFiltered === "scissors" && computerSelection === "paper") ||
+        resultMessage = "Tie! Play again to find out winner"
+    } else if ((playerSelectionFiltered === "scissors" && computerSelection === "paper") ||
         (playerSelectionFiltered === "paper" && computerSelection === "rock") ||
         (playerSelectionFiltered === "rock" && computerSelection === "scissors")
     ) {
-        console.log(`User wins! ${playerSelection} beats ${computerSelection}"`)
-        return "User"
+        resultMessage = `You win! ${playerSelectionFiltered.charAt(0).toUpperCase() + playerSelectionFiltered.slice(1)} beats ${computerSelection}.`;
     }
 
     else {
-        console.log("Computer Wins! Paper beats rock")
-        return "Computer"
+        resultMessage = `You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelectionFiltered}.`;
     }
+
+    alert(resultMessage)
 
 }
 
 function game() {
     console.log("Hi, this is Rock Paper Scissor Game in order to win this game you have to score five points, good luck!")
 
-    let computerPoints = 0;
-    let userPoints = 0;
-    let tiePoints = 0
-    let gameNumber = 1
 
-    while (computerPoints < 5 && userPoints < 5) {
+    const btn_rock = document.getElementById("rock")
+    btn_rock.addEventListener("click", function () {playRound('rock')});
 
-        let choice = prompt(`Game number ${gameNumber}! Choose your choice by typing rock paper o scissors"`);
-        let result = playRound(choice)
-        gameNumber++;
+    const btn_paper = document.getElementById("paper")
+    btn_paper.addEventListener("click", function () {playRound('paper')});
 
-        if (result === "Computer") {
-            computerPoints++;
-        }
-        else if (result === "User") {
-            userPoints++;
-        }
-        else if (result === "User") {
-            tiePoints++;
-        }
+    const btn_scissors = document.getElementById("scissors")
+    btn_scissors.addEventListener("click", function () {playRound('scissors')});
+    // while (computerPoints < 5 && userPoints < 5) {
 
-        console.log(`User Points ${userPoints}!`)
-        console.log(`Computer Points ${computerPoints}!`)
-        console.log(`Tie Points ${tiePoints}!`)
+    //     let choice = prompt(`Game number ${gameNumber}! Choose your choice by typing rock paper o scissors"`);
+    //     let result = playRound(choice)
+    //     gameNumber++;
 
-    }
-    if (computerPoints === 5) {
-        return console.log('Computer Won! User Lost')
-    }
-    else if (userPoints === 5) {
-        return console.log('User Won! Computer Lost')
-    }
+    //     if (result === "Computer") {
+    //         computerPoints++;
+    //     }
+    //     else if (result === "User") {
+    //         userPoints++;
+    //     }
+    //     else if (result === "User") {
+    //         tiePoints++;
+    //     }
+
+    //     console.log(`User Points ${userPoints}!`)
+    //     console.log(`Computer Points ${computerPoints}!`)
+    //     console.log(`Tie Points ${tiePoints}!`)
+
+    // }
+    // if (computerPoints === 5) {
+    //     return console.log('Computer Won! User Lost')
+    // }
+    // else if (userPoints === 5) {
+    //     return console.log('User Won! Computer Lost')
+    // }
 
 }
 
-//game()
+game()
